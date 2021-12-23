88d46cde486b29940bec8ba91eaa3499ec41c381
tolerance-go <892728595@qq.com>
2021-12-23 21:38:47 +0800

    chore: update docs layout

--- .dumi/theme/layout.tsx
+++ .dumi/theme/layout.tsx
@@ -1,14 +1,12 @@
-﻿import { ConfigProvider, Affix, Button, Switch, Space, Input } from '@ty/antd';
+﻿import { ConfigProvider } from '@ty/antd';
 import zhCN from '@ty/antd/es/locale/zh_CN';
-import Form from '@ty/antd/lib/form/Form';
-import FormItem from '@ty/antd/lib/form/FormItem';
 import Layout from 'dumi-theme-default/src/layout';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
-import React, { useMemo, useState } from 'react';
+import React from 'react';
 import { IRouteComponentProps } from 'umi';
-
 import './layout.less';
+
 moment.locale('zh-cn');
 
 /** moment 使用的时区 跟 GTM 相差8小时 重写moment 默认的 toString 方法解决 */
@@ -16,19 +14,14 @@ moment.fn.toISOString = function () {
   return this.format('YYYY-MM-DDTHH:mm:ss.SSS[Z]');
 };
 
-const MockTagCont = '__mock';
-
-const getToken = () => {
-  return localStorage.getItem('tongyu_USER_LOCAL_FIELD');
-};
-
 export default ({ children, ...props }: IRouteComponentProps) => {
-  const mock = !!localStorage.getItem(MockTagCont);
-  const [token, setToken] = useState(getToken());
-
   return (
     <ConfigProvider locale={zhCN}>
-      {location.pathname === '/one-proto' ? children : <Layout {...props}>{children}</Layout>}
+      {window.location.pathname === '/one-proto' ? (
+        children
+      ) : (
+        <Layout {...props}>{children}</Layout>
+      )}
     </ConfigProvider>
   );
 };
